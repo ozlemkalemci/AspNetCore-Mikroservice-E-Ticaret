18da62e8482eeb8013f4b50fb9e6218c70a79f8f
Özlem Kalemci <ozlmklmc@gmail.com>
2025-08-17 22:14:27 +0300

    Created: Entities, Dtos, Settings, Mapping

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class Product
+{
+	[BsonId]
+	[BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+	public string ProductId { get; set; }
+	public string ProductName { get; set; }
+	public decimal Price { get; set; }
+	public decimal ProductImageUrl { get; set; }
+	public string ProductDescription { get; set; }
+	public string CategoryId { get; set; }
+	[BsonIgnore]
+	public Category Category { get; set; }
+}
