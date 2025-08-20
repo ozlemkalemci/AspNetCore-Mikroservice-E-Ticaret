bc6a74a482ca181995f0f7c5ea4d48166fbae8a5
Özlem Kalemci <ozlmklmc@gmail.com>
2025-08-20 22:09:04 +0300

    Completed catalog services and controllers

--- MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/UpdateProductImageDto.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/UpdateProductImageDto.cs
@@ -2,7 +2,7 @@
 {
 	public class UpdateProductImageDto
 	{
-		public string ProductImagesId { get; set; }
+		public string ProductImageId { get; set; }
 		public string Image1 { get; set; }
 		public string Image2 { get; set; }
 		public string Image3 { get; set; }
