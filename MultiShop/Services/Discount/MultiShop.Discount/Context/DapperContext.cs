179677d13bf89ebcfe815653f0eae263d2a7244d
Özlem Kalemci <ozlmklmc@gmail.com>
2025-09-08 21:43:17 +0300

    add discount nugget packages

---
+++ MultiShop/Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultiShop.Discount.Context;
+
+public class DapperContext : DbContext
+{
+	private readonly IConfiguration _configuration;
+	private readonly string _connectionString;
+	public DapperContext(IConfiguration configuration)
+	{
+		_configuration = configuration;
+		_connectionString = _configuration.GetConnectionString("DefaultConnection");
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("");
+	}
+
+}
